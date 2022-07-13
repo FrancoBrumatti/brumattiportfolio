abb6b43aba7430a7f3e13c2b94c7f55d75809d36
Franco Brumatti <franco.brumatti@gmail.com>
2022-07-13 15:59:16 -0300

    minor deploy changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
 import Layout from "../components/Layout";
 import GlobalStyles from "../components/GlobalStyles";
 
-import { motion } from "framer-motion";
-
-function MyApp({ Component, pageProps, router }) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyles />
